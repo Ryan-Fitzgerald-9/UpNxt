4a660b882ef90ca2c18c635408af518a3985a6bc
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2023-12-04 10:23:51 -0500

    Nav updated with user logic

--- upnxt-client/src/App.jsx
+++ upnxt-client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     setUser(userData)
   }
 
-  const handleLogoutSuccess = (userData) => {
+  const handleLogoutSuccess = () => {
     setUser(null)
   }
 
--- upnxt-client/src/components/Nav.jsx
+++ upnxt-client/src/components/Nav.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 
-const Nav = () => {
+const Nav = ({ isAuthenticated, username, handleLogout }) => {
     return (
         <div className="Navbar">
             <Link to="/">Home</Link>
-            <Link to="/Login">Login</Link>
-            <Link to="/Logout">Logout</Link>
-            <Link to="/Register">Create Account</Link>
+            {isAuthenticated ? (
+                <>
+                    <span>{username}</span>
+                    <Link to="/Logout" onClick={handleLogout}>Logout</Link>
+                </>
+            ) : (
+                <>
+                    <Link to="/Login">Login</Link>
+                    <Link to="/Register">Create Account</Link>
+                </>
+            )}
         </div>
     )
 }
